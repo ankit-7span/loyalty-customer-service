84d14c46f82209afa228debec504b3ce16232cb0
ankitdabhi <ankit.dabhi.ext@seamless.se>
2023-04-17 08:42:14 +0530

    code changes added

--- src/main/java/com/loyalty/service/customer/CustomerServiceApplication.java
+++ src/main/java/com/loyalty/service/customer/CustomerServiceApplication.java
@@ -3,10 +3,9 @@ package com.loyalty.service.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
